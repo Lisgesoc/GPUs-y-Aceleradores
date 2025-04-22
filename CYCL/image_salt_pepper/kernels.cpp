7ae46a97c57b078035cc17eee257154ace4a461a
David Díaz Gregorio <152897620+Lisgesoc@users.noreply.github.com>
2025-04-22 18:10:29 +0200

    Add files via upload

--- CYCL/image_salt_pepper/kernels.cpp
+++ CYCL/image_salt_pepper/kernels.cpp
@@ -7,7 +7,7 @@ void remove_noise_SYCL(sycl::queue Q, float *im, float *image_out,
     float thredshold, int window_size,
     int height, int width) {
 
-    int ws2 = (window_size - 1) / 2; // Calcula el radio de la ventana
+    int rad = (window_size - 1) / 2; // Calcula el radio 
     int window_area = window_size * window_size;
 
     Q.submit([&](handler& h) {
@@ -15,23 +15,22 @@ void remove_noise_SYCL(sycl::queue Q, float *im, float *image_out,
             int i = idx[0];
             int j = idx[1];
 
-            // Si es un elemento borde copiamos su valor
-            if (i < ws2 || i >= height - ws2 || j < ws2 || j >= width - ws2) {
+
+            if (i < rad || i >= height - rad || j < rad || j >= width - rad) {
                 image_out[i * width + j] = im[i * width + j];
                 return;
             }
 
             float window[9]; 
             int count = 0;
-            for (int ii = -ws2; ii <= ws2; ++ii) {
-                for (int jj = -ws2; jj <= ws2; ++jj) {
+            for (int ii = -rad; ii <= rad; ++ii) {
+                for (int jj = -rad; jj <= rad; ++jj) {
                     int row = i + ii;
                     int col = j + jj;
                     window[count++] = im[row * width + col];
                 }
             }
 
-            //Ordenamos los valores vecinos
             for (int k = 0; k < window_area - 1; ++k) {
                 for (int l = 0; l < window_area - k - 1; ++l) {
                     if (window[l] > window[l + 1]) {
@@ -42,14 +41,12 @@ void remove_noise_SYCL(sycl::queue Q, float *im, float *image_out,
                 }
             }
 
-            float median = window[window_area / 2];
-            float current = im[i * width + j];
-            float diff = abs(median - current);
-            //Seleccionamos el valor final dependiendo de la diferencia y el umbral
-            if (diff <= thredshold) {
-                image_out[i * width + j] = current;
+            float mediana = window[window_area / 2];
+            float val = im[i * width + j];
+            if ((abs(mediana - val)) <= thredshold) {
+                image_out[i * width + j] = val;
             } else {
-                image_out[i * width + j] = median;
+                image_out[i * width + j] = mediana;
             }
         });
     }).wait(); 
